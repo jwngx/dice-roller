daae734a8d01218b9dc3f50b38d509f45fb78f47
jwngx <pcjwngx@gmail.com>
2026-03-07 20:37:55 +0530

    fixed the logic to display dice

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const mySubmit = document.getElementById('mySubmit');
 const result = document.getElementById('result');
 const diceImage = document.getElementById('diceImage');
 
-const values = [];
-const images = [];
-function rollDice() {
+function rollDice(userInput) {
+    const values = [];
+    const images = [];
     for (let i = 0; i < userInput; i++) {
         const randomValue = Math.floor(Math.random() * 6) + 1;
         values.push(randomValue);
-        images.push(`<img src="svg/${randomValue}.svg">`)
+        images.push(`<img src="dice-svg/${randomValue}.svg" alt="DICE${randomValue}">`)
     }
-    result.textContent=`dice : ${values.join(', ')}`;
+    // result.innerHTML=`dice : ${values.join(', ')}`;
     diceImage.innerHTML=images.join('');
 }
 mySubmit.onclick = function (e) {
     e.preventDefault();
-    userInput = myInput.value;
-    rollDice();
+    const userInput = Number(myInput.value);
+    rollDice(userInput);
 }
